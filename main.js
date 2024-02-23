e396ae68f2f97c0492c45e91cf5fa092b02ed76d
NoureddineMaher <nmaher@rubicon.ma>
2024-02-23 17:19:03 +0100

    create react app with electron

--- main.js
+++ main.js
@@ -7,14 +7,15 @@ function createMainWindow(){
     const mainWindow = new BrowserWindow({
         title : 'SecurityVault',
         width: 1000,
-        height: 600
+        height: 600,
     });
+
+    mainWindow.webContents.openDevTools()
 
     const startUrl = url.format({
-        pathname: path.join(__dirname , 'index.html'),
+        pathname: path.join(__dirname , './app/build/index.html'),
         protocol: 'file',
     });
-
 
     mainWindow.loadURL(startUrl);
 }
